c93bf4c2b988242deaf2f11b9daab5b3cf327ab8
ducnguyen1604 <111528176+ducnguyen1604@users.noreply.github.com>
2023-03-06 23:37:53 +0800

    Add mysql connectors, tomcat server and webapps

--- ebookshop/order_record.sql
+++
@@ -1,9 +0,0 @@
-use ebookshop;
- 
-drop table if exists order_records;
-create table order_records (
-  id          int,
-  qty_ordered int,
-  cust_name   varchar(30),
-  cust_email  varchar(30),
-  cust_phone  char(8));
